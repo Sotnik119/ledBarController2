9d3f34f483609443d664a11180dcc26bc390846f
olegsotnik <sotnik119@gmail.com>
2020-11-03 17:30:03 +0300

    добавлены цвета для лучшего парсинга

--- app/src/main/java/com/donteco/elcledcontroller/LedController.kt
+++ app/src/main/java/com/donteco/elcledcontroller/LedController.kt
@@ -11,10 +11,8 @@ object LedController {
 
     enum class LedColors(val command: String, val color: Int) {
         RED("0x04", Color.rgb(255, 0, 0)),
-        PINK("0x04", Color.rgb(255, 165, 165)), //if pink -> use red led
         GREEN("0x05", Color.rgb(0, 255, 0)),
         BLUE("0x06", Color.rgb(0, 0, 255)),
-        BLACK("0x06", Color.rgb(0, 0, 0)), //if need black -> use blue led
         WHITE("0x07", Color.rgb(255, 255, 255)),
         GRAY("0x07", Color.rgb(140, 140, 140)),
         ORANGE("0x08", Color.rgb(255, 180, 0)),
@@ -29,6 +27,13 @@ object LedController {
         PURPLE2("0x0e", Color.rgb(83, 0, 255)),
         PURPLE3("0x12", Color.rgb(120, 0, 255)),
         PURPLE4("0x16", Color.rgb(120, 85, 255)),
+        /// additional for best parse
+        BLACK("0x06", Color.rgb(0, 0, 0)), //if need black -> use blue led
+        LIGHT_PURPLE("0x0e", Color.rgb(220, 136, 255)), // use purple
+        PINK("0x04", Color.rgb(255, 165, 165)), //if pink -> use red led
+        LIGHT_GREEN("0x05", Color.rgb(155, 255, 136)), // use green
+        LIGHT_BLUE("0x06", Color.rgb(136, 159, 255)), //use blue
+        LIGHT_YELLOW("0x0c", Color.rgb(255, 234, 136)) // use yellow
     }
 
     private const val enableLed = "0x03"
