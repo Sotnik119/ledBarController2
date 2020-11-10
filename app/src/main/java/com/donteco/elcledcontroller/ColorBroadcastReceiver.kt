c29b126d0e3be850afe102298b1293362336b209
olegsotnik <sotnik119@gmail.com>
2020-11-10 15:22:04 +0300

    update

--- app/src/main/java/com/donteco/elcledcontroller/ColorBroadcastReceiver.kt
+++ app/src/main/java/com/donteco/elcledcontroller/ColorBroadcastReceiver.kt
@@ -7,18 +7,6 @@ import android.content.Intent
 class ColorBroadcastReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-//        intent?.extras?.getInt("STATUS")?.let {
-//            when (it) {
-//                0 -> LedController.setColor(LedController.green)
-//
-//                1 -> LedController.setColor(LedController.red)
-//
-//                2 -> LedController.setColor(LedController.yellow)
-//
-//                3 -> LedController.setColor(LedController.blue)
-//
-//            }
-//        }
         intent?.extras?.getString("COLOR")?.let {
             LedController.setClosestColor(ColorUtils.parseJsonColor(it))
         }
